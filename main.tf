de5c1d9b1cf54f2dde7b01b8944830521bd2b351
seshanb <125367985+seshanb@users.noreply.github.com>
2023-12-11 16:28:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "time_sleep" "delay" {
+  depends_on = [azurerm_resource_group.example]
+  duration   = "60s"
+}
