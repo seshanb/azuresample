8e8d62039419c4d45ca40d5d44c47a0c64d9f875
seshanb <125367985+seshanb@users.noreply.github.com>
2023-12-12 10:49:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,14 @@ resource "azurerm_resource_group" "example" {
   location = var.location
 }
 
-resource "time_sleep" "delay" {
-  depends_on = [azurerm_resource_group.example]
-  duration   = "60s"
+resource "null_resource" "previous" {}
+
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [null_resource.previous]
+
+  create_duration = "60s"
+}
+
+resource "null_resource" "next" {
+  depends_on = [time_sleep.wait_30_seconds]
 }
